f003ef3dc67066f5ab9919d8da9b96b1e3a05bc0
XxDavexX <dave.santivanez@gmail.com>
2024-01-08 19:34:36 -0500

    daveacceses

--- src/components/Fotter.js
+++ src/components/Fotter.js
@@ -49,7 +49,7 @@ function Fotter() {
                   <li className="list-inline-item"><a href={whatsappUrl} ><i className="fa fa-twitter"></i></a></li>
                   <li className="list-inline-item"><a  href={whatsappUrl}><i className="fa fa-instagram"></i></a></li>
                   <li className="list-inline-item"><a  href={whatsappUrl}><i className="fa fa-google-plus"></i></a></li>
-                  <li className="list-inline-item"><a  href={whatsappUrl} target="_blank"><i className="fa fa-envelope"></i></a></li>
+                  <li className="list-inline-item"><a  href={whatsappUrl} target="_blank" rel="noreferrer"><i className="fa fa-envelope"></i></a></li>
                 </ul>
               </div>
             </div>
